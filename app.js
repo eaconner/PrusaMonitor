49429a3d920b1caf6f2b2ebd0d0e2ebcf04cd1b9
Eric Conner <Eric@EricConner.net>
2020-05-21 10:52:41 -0400

    Initial server

--- app.js
+++ app.js
@@ -1,26 +1,73 @@
 const SerialPort = require('serialport')
-const fs = require('fs')
 const Readline = SerialPort.parsers.Readline
+const fs = require('fs')
+const http = require('http')
 
 const port = new SerialPort('/dev/ttyACM0', {
     baudRate: 115200
 })
 
+const httpHost = 'localhost';
+const httpPort = 8080;
+
+var status = {
+    isPrinting: false,
+    printComplete: true,
+    fileName: "",
+    fileSize: 0,
+    percent: 0,
+    timeRemaining: 0,
+    timeTotal: 0
+}
+
 const parser = port.pipe(new Readline())
 
-const regex = /NORMAL MODE: Percent done: (?<percent>.*); print time remaining in mins: (?<time>.*)/
+const requestListener = function (req, res) {
+    res.setHeader("Content-Type", "application/json")
+    res.writeHead(200)
+    var jsonData = JSON.stringify(status)
+    res.end(jsonData)
+}
+
+const server = http.createServer(requestListener)
+server.listen(httpPort, httpHost, () => {
+    console.log(`Server is running on http://${httpHost}:${httpPort}`)
+})
+
+const regexFileOpened = /File opened: (.*) Size: (.*)/
+const regexNormalPrint = /NORMAL MODE: Percent done: (.*); print time remaining in mins: (.*)/
 
 parser.on('data', function (data) {
-    var res = data.match(regex)
+    var fileOpened = data.match(regexFileOpened)
+    var normalPrint = data.match(regexNormalPrint)
 
     fs.appendFile('PrusaMonitor.log', data, (err) => {
-        if (err) throw err;
-        console.log(data)
-    });
+        if (err) throw err
+    })
 
-    if(res) {
-        console.log('** Percentage:', res[percent])
-        console.log('** Remaining:', res[time])
+    if (fileOpened) {
+        status.fileName = fileOpened[1]
+        status.fileSize = fileOpened[2]
+        status.isPrinting = true
+        status.printComplete = false
+        console.log(`Opened file: ${status.fileName}`)
+    }
+
+    if (normalPrint) {
+        status.percent = normalPrint[1]
+        status.timeRemaining = normalPrint[2]
+
+        if (status.percent == 100 && !status.printComplete) {
+            status.isPrinting = false
+            status.printComplete = true
+            console.log(`** Print is complete! **`)
+            // @TODO - Notify that print is complete
+        }
+
+        if (status.timeTotal == 0) {
+            status.timeTotal = normalPrint[2]
+        }
+        console.log(`Percent complete: ${status.percent}%, Time Remaining: ${status.timeRemaining} of ${status.timeTotal}`)
     }
 })
 
