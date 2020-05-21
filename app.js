9b099dfc57da76bd67e9969e646d7b005758c51a
Eric Conner <Eric@EricConner.net>
2020-05-21 07:21:58 -0400

    Log serial data to PrusiaMonitor.log

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const SerialPort = require('serialport')
+const fs = require('fs')
 const Readline = SerialPort.parsers.Readline
+
 const port = new SerialPort('/dev/ttyACM0', {
     baudRate: 115200
 })
@@ -9,11 +11,19 @@ const parser = port.pipe(new Readline())
 const regex = /NORMAL MODE: Percent done: (?<percent>.*); print time remaining in mins: (?<time>.*)/
 
 parser.on('data', function (data) {
-  var res = data.match(regex)
-  if(res) {
-      console.log('** Percentage:', res[1])
-      console.log('** Remaining:', res[2])
-  } else {
-      console.log('Data: ', data)
-  }
+    var res = data.match(regex)
+
+    fs.appendFile('PrusaMonitor.log', data, (err) => {
+        if (err) throw err;
+        console.log(data)
+    });
+
+    if(res) {
+        console.log('** Percentage:', res[percent])
+        console.log('** Remaining:', res[time])
+    }
+})
+
+port.on('error', function(err) {
+    console.log(err.message)
 })
